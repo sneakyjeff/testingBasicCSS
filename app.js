21f38bc4db4fd4c6c3cd616bcc47e868ab7f9a41
Jeffrey Trang <Jeffreyhuytrang@yahoo.com>
2018-06-21 22:56:14 -0700

    adding nodejs to practice hosting server

---
+++ app.js
@@ -0,0 +1,3 @@
+var awesome = require('./server');
+
+console.log(awesome.printHello());
---
+++ server.js
@@ -0,0 +1,17 @@
+function countArray(array){
+	return 'this is the length: ' + array.length
+}
+
+function printHello(){
+	return 'hello biches';
+}
+
+var functionexpression = function(){
+	return 'hello my friends';
+}
+
+module.exports = {
+	countArray: countArray, 
+	printHello: printHello
+};
+
